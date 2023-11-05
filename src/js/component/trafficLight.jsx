bdd980e757ce855f9ba45de0dc1edca9d5889fe5
Emilio <136965828+Emdihoy@users.noreply.github.com>
2023-11-05 18:56:51 +0000

    Diseño realizado, funciones por completar

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -1,48 +1,40 @@
 import React, { useState } from "react";
 
+
 const trafficLight = () => {
 
     const [ redLight, setredLight] = useState("red");
     const [ yellowLight, setyellowLight ]= useState("yellow");
     const [ greenLight, setgreenLight ]= useState("green");
-    const [glowLight, setglowLight] = useState("glowLight")
-
-
-     function redChange() {
-        
-         if (redLight === "red"){
-          setredLight('blue' )
-         }else {
-          setredLight('red')
-         }
-      }
-
-      function yellowChange() {
-        
-        if (yellowLight === "yellow"){
-          setyellowLight('blue' )
-        }else {
-          setyellowLight('yellow')
-        }
-     }
-
-     function greenChange() {
-        
-      if (greenLight === "green" ){
-        setgreenLight('blue' )
-      }else {
-        setgreenLight('green')
+ 
+    function redChange() {
+      if (redLight === "red"){
+        setredLight('glow')
+     }else {
+      setredLight('red')
       }
    }
+    function yellowChange() {
+          if (yellowLight === "yellow"){
+            setyellowLight('glow' )
+         }else {
+           setyellowLight('yellow')
+          }
+       }
+    function greenChange() {
+        if (greenLight === "green"){
+          setgreenLight('glow' )
+        }else {
+         setgreenLight('green')
+      }
+     }
 	
     return (
 
-		<div className="text-center">
-
-            <button className={redLight} onClick= {()=>redChange()} style={{background: redLight}}>Rojo</button>
-            <button className={yellowLight} onClick= {()=>yellowChange()} style={{background: yellowLight}}>Amarillo</button>
-            <button className={greenLight} onClick= {()=>greenChange()} style={{background: greenLight}}>Verde</button>
-			
+		<div className="trafficLight">
+      <button className={redLight} onClick= {()=>redChange()} style={{background: redLight}}></button>
+      <button className={yellowLight} onClick= {()=>yellowChange()} style={{background: yellowLight}}></button>
+      <button className={greenLight} onClick= {()=>greenChange()} style={{background: greenLight}}></button>
 		</div>
 	);
 };
